bd1207db2c31ac08d5806ab3a65ec41fea3f66d8
Maleao <mtdevjf@gmail.com>
2024-02-29 08:05:39 -0300

    Criação da aba Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./styles/App.css";
-import { Container, Paper, Box, Grid, TextField, IconButton, Button, Typography } from "@material-ui/core";
+import { Container, Paper, Box, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import GridComponent from "./Components/GridComponent.js";
 import TeamButtons from "./Components/TeamButtons.js";
 import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import FileUploadButton from "./Components/FileUploadButton.js";
+import AbaUpdate from "./Components/AbaUpdate.js";
+
 
 /****************************************STYLE*************************************************/
 const useStyles = makeStyles((theme) => ({
@@ -18,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
   },
   inputGroup: {
     marginBottom: theme.spacing(3),
+    marginTop: theme.spacing(3)
   },
   pageTitle: {
     marginBottom: theme.spacing(5),
@@ -27,40 +30,125 @@ const useStyles = makeStyles((theme) => ({
     color: '#0066cc',
     textAlign: 'center',
   },
+  tabButton: {
+    marginBottom: theme.spacing(4),
+  },
 }));
 /********************************************************************************************/
 
 /*****************************************FUNCTIONS********************************************/
+
+
+
+
 function App() {
   const classes = useStyles();
+  const [showAlert, setShowAlert] = useState(true);
+  const [mostrarNovaAba, setMostrarNovaAba] = useState(false);
   const layoutTemplate = { name: "", pinicial: "", pfinal: "", size: "" };
   const [layouts, setLayout] = useState([layoutTemplate,]);
-  const [showAlert, setShowAlert] = useState(true);
 
- const addRows = (isDateField) => {
-  const newField = isDateField ? { date: new Date() } : layoutTemplate;
-  setLayout([...layouts, newField]);
-};
 
-  // Função para validar entrada numérica
+  const addRows = (isDateField) => {
+    const newField = isDateField ? { date: new Date() } : layoutTemplate;
+    setLayout([...layouts, newField]);
+  };
+
   const handleNumericInput = (e) => {
     const value = e.target.value;
-    // Substituir não números por uma string vazia
     const numericValue = value.replace(/\D/g, '');
-    // Atualizar o valor no campo
     e.target.value = numericValue;
   };
 
-  // Atualiza campo na array
   const changeField = (e, index) => {
     const updatedField = layouts.map((field, i) => index === i ?
       { ...field, [e.target.name]: e.target.value } : field);
     setLayout(updatedField);
   };
 
-  // Função para ser usada nos campos de entrada
   const onChange = (e, index) => {
-    // Chamando ambas as funções
+    handleNumericInput(e);
+    changeField(e, index);
+  };
+
+  const removeField = (index) => { 
+    const filteredFields = [...layouts];
+    filteredFields.splice(index, 1);
+    setLayout(filteredFields)
+  }
+
+
+  // Função para alternar entre a aba existente e a nova aba
+  const alternarAba = () => {
+    setMostrarNovaAba((prevState) => !prevState);
+  };
+
+  return (
+    <Container className={classes.root}>
+      <Paper component={Box} p={4}>
+        {showAlert && (
+          <Alert severity="warning" onClose={() => setShowAlert(false)}>
+            <AlertTitle>Atenção</AlertTitle>
+            Favor gerar os campos em conformidade com a 
+            documentação da Operadora
+          </Alert>
+        )}
+
+        <Typography variant="h4" className={classes.pageTitle}>
+          Gerador de Layout
+        </Typography>
+
+        <Button classes={classes.marginButton}
+        variant="contained" 
+          color="default"
+          className={classes.tabButton}
+          onClick={alternarAba}
+        >
+        {mostrarNovaAba ? 'Voltar Para Criação' : 'Atualizar layout'}
+        </Button>
+          
+        {mostrarNovaAba ? layouts.map((layout, index) => (<AbaUpdate
+               key={index}
+               layout={layout}
+               index={index}
+               changeField={changeField}
+               onChange={onChange}
+               removeField={removeField}
+               addRows={addRows}t
+               /> 
+        ))
+          : <AbaPrincipal /> }
+          {mostrarNovaAba == true ? <TeamButtons addRows={addRows} /> :  null}
+      </Paper>
+    </Container>
+  );
+}
+
+// Componente para a aba existente
+const AbaPrincipal = ({}) => {
+  const layoutTemplate = { name: "", pinicial: "", pfinal: "", size: "" };
+  const [layouts, setLayout] = useState([layoutTemplate,]);
+
+  
+
+  const addRows = (isDateField) => {
+    const newField = isDateField ? { date: new Date() } : layoutTemplate;
+    setLayout([...layouts, newField]);
+  };
+
+  const handleNumericInput = (e) => {
+    const value = e.target.value;
+    const numericValue = value.replace(/\D/g, '');
+    e.target.value = numericValue;
+  };
+
+  const changeField = (e, index) => {
+    const updatedField = layouts.map((field, i) => index === i ?
+      { ...field, [e.target.name]: e.target.value } : field);
+    setLayout(updatedField);
+  };
+
+  const onChange = (e, index) => {
     handleNumericInput(e);
     changeField(e, index);
   };
@@ -72,46 +160,26 @@ function App() {
   }
 
   const handleFileUpload = (files) => {
-    // Parte para enviar para o servidor os arquivos
     console.log('Arquivos:', files);
   }
 
-/********************************************************************************************/
 
   return (
-    <Container className={classes.root}>
-      <Paper component={Box} p={4}>
-
-      {showAlert && (
-          <Alert severity="warning" onClose={() => setShowAlert(false)}>
-            <AlertTitle>Atenção</AlertTitle>
-            O gerador de Layout segue uma sequência, favor gerar os campos em conformidade com a 
-            documentação da Operadora
-          </Alert>
-        )}
-
-        <Typography variant="h4" className={classes.pageTitle}>
-          Gerador de Layout
-        </Typography>
-
-        {layouts.map((layout, index) => (
-          <GridComponent
-            key={index}
-            layout={layout}
-            index={index}
-            changeField={changeField}
-            onChange={onChange}
-            removeField={removeField}
-          />
-        ))}
-
-        <TeamButtons addRows={addRows} />
-        <FileUploadButton onFileUpload={handleFileUpload} />
-
-
-      </Paper>
-    </Container>
-  );
+    <>
+      {layouts.map((layout, index) => (
+        <GridComponent
+          key={index}
+          layout={layout}
+          index={index}
+          changeField={changeField}
+          onChange={onChange}
+          removeField={removeField}
+        />
+      ))}
+      <FileUploadButton onFileUpload={handleFileUpload}  />
+      <TeamButtons addRows={addRows} />
+    </>
+   )
 }
 
 export default App;
---
+++ src/Components/AbaUpdate.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import { Grid, TextField, IconButton, MenuItem } from "@material-ui/core";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import EditIcon from "@material-ui/icons/Edit";
+import SaveIcon from "@material-ui/icons/Save";
+import TeamButtons from "./TeamButtons";
+
+const AbaUpdate = ({ layout, index, changeField, onChange, removeField, addRows }) => {
+  const [dateFormat, setDateFormat] = useState('dd/MM/yyyy');
+  const [dados, setDados] = useState({
+    nome: "",
+    pontoInicial: "",
+    pontoFinal: "",
+    tamanho: ""
+  });
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    // Aqui você pode fazer uma chamada para o backend para obter os dados e atualizar o estado
+    // Por exemplo:
+    // fetchDadosDoBackEnd().then((response) => setDados(response));
+    // Isso é apenas um exemplo. Certifique-se de implementar de acordo com sua lógica de backend.
+  }, []);
+  
+  const handleEdit = () => {
+    setEditMode(!editMode);
+  };
+  
+  const handleChange = (event, field) => {
+    setDados({
+      ...dados,
+      [field]: event.target.value
+    });
+  };
+
+  const handleSave = () => {
+    // Implemente a lógica para salvar os dados editados
+    console.log("Dados salvos:", dados);
+    setEditMode(false);
+  };
+
+  const handleDateFormatChange = (event) => {
+    setDateFormat(event.target.value);
+  };
+
+
+  // Lista de formatos de data
+  const dateFormats = [
+    { label: 'dd/MM/yyyy', value: 'dd/MM/yyyy' },
+    { label: 'MM/dd/yyyy', value: 'MM/dd/yyyy' },
+    { label: 'yyyy/MM/dd', value: 'yyyy/MM/dd' },
+    { label: 'dd-MM-yyyy', value: 'dd-MM-yyyy' },
+    { label: 'MM-dd-yyyy', value: 'MM-dd-yyyy' },
+    { label: 'yyyy-MM-dd', value: 'yyyy-MM-dd' },
+    { label: 'dd.MM.yyyy', value: 'dd.MM.yyyy' },
+    { label: 'MM.dd.yyyy', value: 'MM.dd.yyyy' },
+    { label: 'yyyy.MM.dd', value: 'yyyy.MM.dd' },
+  ];
+
+  return (
+    <Grid container spacing={3} key={index}>
+      {layout && layout.date ? (
+        <Grid item xs={12} md={3}>
+          <TextField
+            select
+            label="Formato da Data"
+            variant="outlined"
+            fullWidth
+            value={dateFormat}
+            onChange={handleDateFormatChange}
+            disabled={!editMode}
+          >
+            {dateFormats.map((format, index) => (
+              <MenuItem key={index} value={format.value}>
+                {format.label}
+              </MenuItem>
+            ))}
+          </TextField>    
+        </Grid>
+      ) : (
+        <>
+          <Grid item xs={12} sm={6} md={3}>
+            <TextField
+              label="Nome"
+              placeholder="Nome"
+              variant="outlined"
+              fullWidth
+              name="name"
+              onChange={(e) => changeField(e, index)}
+              disabled={!editMode}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              label="Ponto Inicial"
+              placeholder="Ponto Inicial"
+              variant="outlined"
+              fullWidth
+              name="pinicial"
+              onChange={(e) => onChange(e, index)}
+              disabled={!editMode}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              label="Ponto Final"
+              placeholder="Ponto Final"
+              variant="outlined"
+              fullWidth
+              name="pfinal"
+              onChange={(e) => onChange(e, index)}
+              disabled={!editMode}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              label="Tamanho"
+              placeholder="Tamanho"
+              variant="outlined"
+              fullWidth
+              name="size"
+              onChange={(e) => onChange(e, index)}
+              disabled={!editMode}
+            />
+          </Grid>
+        </>
+      )}
+      
+      <Grid item xs={10} sm={3} md={1}>
+        {editMode ? (
+          <IconButton color="primary" onClick={handleSave}>
+            <SaveIcon />
+          </IconButton>
+        ) : (
+          <IconButton color="primary" onClick={handleEdit}>
+            <EditIcon />
+          </IconButton>
+        )}
+      </Grid>
+
+      <Grid item xs={12} sm={6} md={1}>
+        <IconButton color="secondary" onClick={() => removeField(index)}>
+          <DeleteOutlineIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AbaUpdate;
